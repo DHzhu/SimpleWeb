6ee83c124701c2cb904f2474545988b2202ed6a1
DHzhu <longhunguoshang@hotmail.com>
2017-07-10 11:24:47 +0800

    push codes

---
+++ src/main/java/com/controller/IndexController.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @desc  : TODO
+ * @author: Zhu
+ * @date  : 2017年7月4日
+ */
+@SuppressWarnings("restriction")
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value = "/index.do")
+	public String showIndex(HttpServletRequest request, Model model){
+
+		return "index";
+	}
+}
