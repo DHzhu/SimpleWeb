4cc74cf41a3b028031c156ad9696e3d5c669092b
DHzhu <longhunguoshang@hotmail.com>
2017-11-20 11:09:07 +0800

    push codes

---
+++ src/main/java/com/controller/PicController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @desc  : TODO
+ * @author: Zhu
+ * @date  : 2017年11月20日
+ */
+@Controller
+public class PicController {
+
+}
